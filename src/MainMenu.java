3b93010d24cce3b9bd80a92133adf223e54b3016
Kathir Dev <121499256+kwto@users.noreply.github.com>
2023-10-17 09:03:07 -0500

    Maven and MainMenu
    added Maven compiler and added the basic parts of the main interface

---
+++ src/MainMenu.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MainMenu {
+    private JPanel panel1;
+    private JCheckBox Cb1;
+    private JCheckBox Cb2;
+    private JCheckBox Cb3;
+    private JCheckBox Cb4;
+    private JTextField Tf1;
+    private JTextField Tf2;
+    private JTextField Tf3;
+    private JTextField Tf4;
+}
